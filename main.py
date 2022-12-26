592e96b0754a5894ce901c186e1060d4b192ab52
Pavel <pavelsoll2@gmail.com>
2022-12-26 16:41:51 +0300

    Теперь только на русском. добавил import re и messagebox from tkinter. Обновил комментарии к функциям. Функция по поиску фото теперь find_photos. Теперь запрашивается не только путь, но и имя собеседника, а также, сам путь теперь нужен не к папке с сообщениями а к самому архиву, а функция use_path уже сама найдет нужный файл для поиска сообщений и фото в них. Доабвил raise ошибок в GUI.

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import os
 from bs4 import BeautifulSoup
 from os import walk
 import requests
 import tkinter as tk
+from tkinter import messagebox
+import re
 
 
 # Парсит html файл и возвращает список ссылок на фото
-def find_photo(file):
+def find_photos(file):
 	links = []
 
 	with open(file, 'r') as page:
@@ -17,17 +20,18 @@ def find_photo(file):
 	return links
 
 
-# Возвращает номер сообщения
+# Возвращает номер страницы
 def page_num(page):
 	return int(page.replace('messages', '').replace('.html', ''))
 
 
 # Проходится по файлам в директории и парсит каждый из них,
-# сохраняя все фото из ссылок, что он найдет в папке "Content" в "png." формате
+# сохраняя все фото из ссылок, что он найдет в "png." формате в папке с именем собеседника, созданной в папке "Content"
 def content_saver(path):
+	os.mkdir(f'Content\\{name.get()}')
 	for root, dirs, files in walk(path):
 		for filename in sorted(files, key=page_num):
-			links = find_photo(file=f'{path}/{filename}')
+			links = find_photos(file=f'{path}/{filename}')
 
 			if links:
 				count = 0
@@ -35,14 +39,28 @@ def content_saver(path):
 					link = requests.get(link)
 					count += 1
 
-					with open(f'Content\\{filename}({count}).png', 'wb') as photo:
+					with open(f'Content\\{name.get()}\\{filename}({count}).png', 'wb') as photo:
 						photo.write(link.content)
 			else:
 				continue
 
 
+# Ищет в веденном пути к архиву папку messages и в файле index-messages.html ищет введенное имя собеседника
 def use_path():
-	content_saver(e.get())
+	try:
+		with open(f'{archive_path.get()}\messages\index-messages.html', 'r') as page:
+			messages = page.read()
+
+			try:
+				num = re.findall(rf'<a href="(.+)(/messages0.html">)({name.get()})(</a>)', messages)[0][0]
+				path = f'{archive_path.get()}\\messages\\{num}'
+				content_saver(path)
+
+			except IndexError:
+				messagebox.showerror('Ошибка', 'Указано неверное имя собеседника')
+
+	except FileNotFoundError:
+		messagebox.showerror('Ошибка', 'Указан неверный путь к архиву')
 
 
 # GUI
@@ -55,12 +73,19 @@ if __name__ == '__main__':
 
 	root.resizable(width=False, height=False)
 
-	tk.Label(root, text="Введите путь к папке: ", font='Roboto', background='#fafafa').grid()
+	tk.Label(root, text="Введите путь к архиву: ", font='Roboto', background='#fafafa').grid()
+	archive_path = tk.Entry(root, width=50)
+	archive_path.grid(row=0, column=1)
 
-	e = tk.Entry(root, width=50)
-	e.grid(row=0, column=1)
+	tk.Label(root, text="Введите имя собеседника: ", font='Roboto', background='#fafafa').grid()
+	global name
+	name = tk.Entry(root, width=50)
+	name.grid(row=1, column=1)
 
-	btn = tk.Button(root, text='Submit', font='Roboto', command=use_path)
-	btn.grid(row=1, column=0, pady=10)
+	btn = tk.Button(root, text='Старт', font='Roboto', command=use_path)
+	btn.grid(row=2, column=0, pady=10)
 
+	btn2 = tk.Button(root, text='Стоп', font='Roboto', command=root.destroy)
+	btn2.grid(row=2, column=1, pady=10)
 	root.mainloop()
+
